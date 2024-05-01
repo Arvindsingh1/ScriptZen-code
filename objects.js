8ed59f6ec5ca268d7df2ac4fbaf9cd000dbd7baf
Arvind <akash.barauli.as@gmail.com>
2024-05-01 21:54:12 +0530

    events

--- objects.js
+++ objects.js
@@ -1,43 +1,54 @@
-const info = [
-  {
-    name: "Arvind",
-    age: 25,
-    gender: "male",
-    address: {
-        city: {
-          village: "nalaSopara",
-          post: "Thane",
-        },
-        state: "maharashtra",
-      },
-  },
+// const info = [
+//   {
+//     name: "Arvind",
+//     age: 25,
+//     gender: "male",
+//     address: {
+//         city: {
+//           village: "nalaSopara",
+//           post: "Thane",
+//         },
+//         state: "maharashtra",
+//       },
+//   },
 
-  {
-    name: "ritik",
-    age: 25,
-    gender: "male",
-    address: {
-      city: {
-        village: "nalaSopara",
-        post: "Thane",
-      },
-      state: "maharashtra",
-    },
-  },
-];
+//   {
+//     name: "ritik",
+//     age: 25,
+//     gender: "male",
+//     address: {
+//       city: {
+//         village: "nalaSopara",
+//         post: "Thane",
+//       },
+//       state: "maharashtra",
+//     },
+//   },
+// ];
 
 // console.log(info[1].gender);
 
-
 // console.log(info[2].address.city.post);
 
-let eslintConfig= {
+// let eslintConfig= {
 
-    "extends": [
-      "react-app",
-      "react-app/jest"
-    ]
-}
+//     "extends": [
+//       "react-app",
+//       "react-app/jest"
+//     ]
+// }
 
 console.log(eslintConfig.extends[1])
+
+const origin = {
+  name: "Arvind",
+  age: 25,
+  gender: "male",
+  address: null,
+};
+
+const output = origin.address;
+
+// console.log(!output)
+
 
